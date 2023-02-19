06d2fba201ee1e69faa7c481295bbab4421c2bc0
LongjunXU <xulongjun.fr@gmail.com>
2023-02-20 00:18:16 +0100

    Add project files.

---
+++ CopyFileTool/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3;
+
+Console.WriteLine("Hello, World!");
+
+var srcLocation = @"C:\Users\xulon\Desktop\RPT";
+var destLocation = @"C:\Users\xulon\Desktop\Test\";
+
+var srcLocationFile = @"C:\Users\xulon\Desktop\RPT\DEV\ATHLON_VEG_470 Futures restitutions.rpt";
+
+var destLocationFile = @"C:\Users\xulon\Desktop\Test\DEV\ATHLON_VEG_470 Futures restitutions.rpt";
+
+new Tools().CopyAllFiles(srcLocation, destLocation);
+
+new Tools().CopyOneFile(srcLocationFile, destLocationFile);
---
+++ CopyFileTool/Tools.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public static class Tools
+    {
+        public static void CopyAllFiles(string sourceDirectory, string targetDirectory)
+        {
+            // Copy all the files in the current directory
+            foreach (string file in Directory.GetFiles(sourceDirectory))
+            {
+                string fileName = Path.GetFileName(file);
+                string target = Path.Combine(targetDirectory, fileName);
+
+                if (File.Exists(target))
+                {
+                    File.Copy(file, target, true);
+                }
+                else
+                {
+                    File.Copy(file, target);
+                }
+            }
+
+            // Recursively copy files in all subdirectories
+            foreach (string directory in Directory.GetDirectories(sourceDirectory))
+            {
+                string directoryName = Path.GetFileName(directory);
+                string targetSubDirectory = Path.Combine(targetDirectory, directoryName);
+                if(!Directory.Exists(targetSubDirectory))
+                {
+                    Directory.CreateDirectory(targetSubDirectory);
+                }
+                CopyAllFiles(directory, targetSubDirectory);
+            }
+        }
+
+        public static void CopyOneFile(string source, string target)
+        {
+            var targetDirectory = Path.GetDirectoryName(target);
+
+            if (targetDirectory != null && !Directory.Exists(targetDirectory))
+            {
+                Directory.CreateDirectory(targetDirectory);
+            }
+
+            if (File.Exists(target))
+            {
+                File.Copy(source, target, true);
+            }
+            else
+            {
+                File.Copy(source, target);
+            }
+        }
+    }
+}
